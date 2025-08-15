bd9d93e7dd9b6dd36ece88e08e526c355452a904
Melbin J Paulose <91303803+melbinjp@users.noreply.github.com>
2025-08-15 19:12:14 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ const urlsToCache = [
   '/',
   '/index.html',
   '/style.css',
-  '/script.js',
+  '/js/main.js',
   '/tools.json',
   'https://fonts.googleapis.com/css2?family=Montserrat:wght@600;700&family=Open+Sans:wght@400;500&display=swap',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
